57cdd3e14589da7610ba388dfa2c6d52aac52fc0
duda-arch <andre06carvalho@gmail.com>
2021-02-19 11:35:51 -0300

    inicio Back and do formulario

--- cadastro.php
+++ cadastro.php
@@ -4,39 +4,39 @@
 </head>
 <body>
 
-	
-			<form action="cadastro.php?v=1" class="testeq"  method="post">
+				
+											<form action="cadastro.php?v=1" class="testeq"  method="post">
 
-				 <p>Entre com o seu nome</p>
-				 	 <input class="" type="text" name="nome">
+												 <p>Entre com o seu nome</p>
+												 	 <input class="" type="text" name="nome">
 
-				 <p>Entre com seu email</p>
-					 <input class="" type="text" name="email">
+												 <p>Entre com seu email</p>
+													 <input class="" type="text" name="email">
 
-					<p>Entre com seu CPF</p>
-						<input class="" type="int" name="cpf">
+													<p>Entre com seu CPF</p>
+														<input class="" type="int" name="cpf">
 
-					<p>Entre com o seu endereço</p>
-				 	  <input class="" type="text" name="endereco">
+													<p>Entre com o seu endereço</p>
+												 	  <input class="" type="text" name="endereco">
 
-				 	<p>Entre com o sua idade</p>
-				 	  <input class="" type="text" name="idade">
+												 	<p>Entre com o sua idade</p>
+												 	  <input class="" type="text" name="idade">
 
-				 	<p>Crie um loguin </p>
-				 	  <input class="" type="text" name="log">
+												 	<p>Crie um loguin </p>
+												 	  <input class="" type="text" name="log">
 
-				 	<p>Crie uma senha </p>
-				 	  <input class="" type="password" name="senha1">
+												 	<p>Crie uma senha </p>
+												 	  <input class="" type="password" name="senha1">
 
-					<p>Confime sua senha </p>
-					<input class="" type="password" name="senha2">
+													<p>Confime sua senha </p>
+													<input class="" type="password" name="senha2">
 
-					<input  class="button" type="submit" name="enviar" id="botao" value="enviar">
-			</form>
+													<input  class="button" type="submit" name="enviar" id="botao" value="enviar">
+											</form>
 
-			           <?php 
+			           <?php  //cria uma funcao para confirmar senha usaremos mais pra frente tbm no formulario dos funcionarios que sera diferende obviamente dos demais
 
-			         function confirmacao($senha1,$senha2){if($senha1 == null && $senha2== null) {
+			         					function confirmacao($senha1,$senha2){if($senha1 == null && $senha2== null) {
 
 			           					echo "Senha nula";
 											
