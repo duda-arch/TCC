cad55b74f9847fda309d6d358d2dd4182f9895b2
duda-arch <andre06carvalho@gmail.com>
2021-06-08 23:36:54 -0300

    Atualizando Page_Dark

--- pg-dark.php
+++ pg-dark.php
@@ -32,10 +32,7 @@
 		       </li>
 
 		    </ul>
-		    <form class="form-inline my-2 my-lg-0">
-		      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-		      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-		    </form>
+		    
 		</div>
 </nav>
 
@@ -61,9 +58,8 @@
 
 			 <h2 class="fonte_titulo">Os efeitos colaterais do uso de trembolona</h2>
 
-			<li>O aumento das células vermelhas provocado pela trembolona é um fator muito perigoso, pois eleva pressão arterial, o que pode levar à morte. Níveis de colesterol ruim são aumentados, enquanto o colesterol saudável é inibido.</li>
+			<li>Entre eles estão, o crescimento de pelos, acne no rosto e nas costas (devido ao aumento da produção de sebo), diminuição das mamas e perda de cabelo. Além disso, há consequências definitivas para seu corpo, como o aumento do clitóris e o engrossamento da voz</li>
 
-			<li>Aos homens, a droga pode provocar ginecomastia, o crescimento das mamas. Esse é um fator de preocupação grave pois, uma vez estimuladas, as glândulas mamárias não se reduzem, e você precisará de cirurgia.</li>
 </div>
 		</div>
     </div>
@@ -76,8 +72,8 @@
 <div class="row justify-content-center">
   <div class="col-4  ">
     <div class="list-group" id="list-tab" role="tablist">
-      <a class="list-group-item group2  fonte ist-group-item-action active">Trembolona</a>
-      <img src="imagens/trembolona.jpg" class="img-fluid img-thumbnail sumiu" style="height: 300px;">
+      <a class="list-group-item group2  fonte ist-group-item-action active">Testosterona</a>
+      <img src="imagens/testosterona.jpg" class="img-fluid img-thumbnail sumiu" style="height: 300px;">
     </div>
   </div>
   <div class="col-8 ">
@@ -85,13 +81,13 @@
   <div class="desc_pro1">
     <div class="tab-content descric_product" id="nav-tabContent">
 
-    	<h2 class="fonte_titulo">Os benefícios da trembolona ao corpo</h2>
+    	<h2 class="fonte_titulo">Testosterona</h2>
 
-			<li>O efeito catabólico (ou degradação muscular) é quase completamente inibido devido à diminuição que a droga causa na sensibilidade ao cortisol, hormônio responsável pela catabólise muscular.</li>
+			<li>A testosterona e outros andrógenos tem efeito em praticamente todos os tecidos. Estas ações metabólicas são do tipo anabolizante e adquire alta importância fisiológica. </li>
 
-			<li>O hormônio do crescimento IGF-I é aumentado drasticamente. Esse tipo do hormônio é muito semelhante à insulina tipo 1, e promove aumento de força e volume muscular. A retenção de nitrogênio que a droga promove no organismo também provoca aumento do número de células musculares.</li>
+			<li>Favorecem a retenção de nitrogênio (balanço nitrogenado positivo) a nível de todos os tecidos, particularmente do músculo esquelético, do coração, do osso e do tecido colágeno em geral. </li>
 
-			 <h2 class="fonte_titulo">Os efeitos colaterais do uso de trembolona</h2>
+			 <h2 class="fonte_titulo">Os efeitos colaterais do uso de Testosterona</h2>
 
 			<li>O aumento das células vermelhas provocado pela trembolona é um fator muito perigoso, pois eleva pressão arterial, o que pode levar à morte. Níveis de colesterol ruim são aumentados, enquanto o colesterol saudável é inibido.</li>
 
